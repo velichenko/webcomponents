4cbb5ed0af0ca41d64af9ef8b01ad8f7ce813a1c
b.medvedev <b.medvedev@icloud.com>
2018-12-08 16:50:51 +0300

    Todo to func Component

--- client/src/pages/Todos/Todo.jsx
+++ client/src/pages/Todos/Todo.jsx
@@ -1,8 +1,8 @@
-import React, {Component, Fragment} from 'react';
+import React, {Fragment} from 'react';
 import Input from "../../components/Input";
 import {connect} from "react-redux";
 import {editTodoField, removeTodo, updateTodo} from "../../redux/reducers/todos";
-import validator from "./validator";
+import checker from "./validator";
 
 const connector = connect(
     null,
@@ -10,65 +10,59 @@ const connector = connect(
         removeTodo: dispatch(removeTodo),
         editTodoField: dispatch(editTodoField),
         updateTodo: dispatch(updateTodo)
-    }),
-    null
+    })
 );
 
-class Todo extends Component {
-    render() {
-        const {_id, isEditing, title, errors} = this.props.todo;
-        return (
-            <div>
-                {
-                    isEditing ?
-                        <Fragment>
-                            <Input
-                                value={title}
-                                onChange={value => this.props.editTodoField(_id, 'title', value)}
-                                onBlur={this.validator}
-                                error={errors.title}
-                            />
+const Todo = props => {
+    const {_id, isEditing, title, errors} = props.todo;
+    const validator = () => {
+        const {errors} = checker({title});
 
-                            <button onClick={this.saveHandler}>
-                                Сохранить
-                            </button>
-                        </Fragment>
-                        :
-                        <Fragment>
-                            {title}
-
-                            <button
-                                onClick={() => this.props.editTodoField(_id, 'isEditing', !isEditing)}
-                            >
-                                Изменить
-                            </button>
-                        </Fragment>
-                }
-
-                <button onClick={() => this.props.removeTodo(_id)}>Удалить</button>
-            </div>
-        );
-    }
-
-    validator = () => {
-        const {title, _id} = this.props.todo;
-        const {errors} = validator({title});
-
-        return this.props.editTodoField(_id, 'errors', errors);
+        return props.editTodoField(_id, 'errors', errors);
     };
-
-    saveHandler = e => {
+    const saveHandler = e => {
         e.preventDefault();
 
-        const {title, _id} = this.props.todo;
-        const {errors, isValid} = validator({title});
+        const {errors, isValid} = checker({title});
 
         if (!isValid) {
-            return this.props.editTodoField(_id, 'errors', errors);
+            return props.editTodoField(_id, 'errors', errors);
         }
 
-        return this.props.updateTodo(_id, title);
-    }
-}
+        return props.updateTodo(_id, title);
+    };
+
+    return (
+        <div>
+            {
+                isEditing ?
+                    <Fragment>
+                        <Input
+                            value={title}
+                            onChange={value => props.editTodoField(_id, 'title', value)}
+                            onBlur={validator}
+                            error={errors.title}
+                        />
+
+                        <button onClick={saveHandler}>
+                            Сохранить
+                        </button>
+                    </Fragment>
+                    :
+                    <Fragment>
+                        {title}
+
+                        <button
+                            onClick={() => props.editTodoField(_id, 'isEditing', !isEditing)}
+                        >
+                            Изменить
+                        </button>
+                    </Fragment>
+            }
+
+            <button onClick={() => props.removeTodo(_id)}>Удалить</button>
+        </div>
+    );
+};
 
 export default connector(Todo);
